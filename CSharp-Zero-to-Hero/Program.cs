9021e262e4f4069bef7678b1ade8b613022bf2a0
Jay Prakash <jayprakashthakur91@gmail.com>
2025-10-02 15:18:24 +0530

    updated identifier

--- CSharp-Zero-to-Hero/Program.cs
+++ CSharp-Zero-to-Hero/Program.cs
@@ -1,11 +1,12 @@
 ﻿using CSharp_Zero_to_Hero.Chapter03_HelloWorld;
+using CSharp_Zero_to_Hero.Chapter05_Identifier_DataType;
 using System;
 using System.ComponentModel.Design;
 
 public class Program
 {
-	public static void Main()
-	{
-		HelloWorld helloWorld = new HelloWorld();
-	}
+	//public static void Main()
+	//{
+	//	Identifier identifier = new Identifier();
+	//}
 }
