b6245a280ecdd5a0a12f89ca736d8c9b49fca567
Jay Prakash <jayprakashthakur91@gmail.com>
2025-10-02 15:11:09 +0530

    updated program hello world

---
+++ CSharp-Zero-to-Hero/Chapter03_HelloWorld/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Zero_to_Hero.Chapter03_HelloWorld
+{
+    public class HelloWorld
+    {
+        public HelloWorld() 
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ CSharp-Zero-to-Hero/Program.cs
@@ -0,0 +1,11 @@
+﻿using CSharp_Zero_to_Hero.Chapter03_HelloWorld;
+using System;
+using System.ComponentModel.Design;
+
+public class Program
+{
+	public static void Main()
+	{
+		HelloWorld helloWorld = new HelloWorld();
+	}
+}
