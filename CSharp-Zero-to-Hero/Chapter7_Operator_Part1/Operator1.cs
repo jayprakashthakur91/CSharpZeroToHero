1fbcac01af40376e49f3dc9d9881933a7f9b8046
Jay Prakash <jayprakashthakur91@gmail.com>
2025-10-09 20:20:42 +0530

    Operator_part1

---
+++ CSharp-Zero-to-Hero/Chapter7_Operator_Part1/Operator1.cs
@@ -0,0 +1,43 @@
+﻿using CSharp_Zero_to_Hero.Chapter02_Tool;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Zero_to_Hero.Chapter7_Operator_Part1
+{
+    public class Operator1
+    {
+        public  static void Main()
+        {
+            //Write a C# program to input two numbers and perform addition and subtraction.Use both normal arithmetic operators (+, -) and compound assignment (+=, -=). 
+            //Example:
+            //Input → 10, 5
+            //Output → Sum = 15, Difference = 5
+            //Write a program to multiply and divide two numbers entered by the user.Use *, /, and /= operators to show how assignment works.
+            //Example:
+            //Input → 20, 4
+            // output Product = 80, Quotient = 5
+            //Write a C# program to find the average of three numbers.Use + and / operators, and show how to use += to accumulate the sum
+            //Example:
+            //Input → 20, 4
+            //Output → Product = 80, Quotient = 5
+            //Write a C# program that takes an integer and prints its square and cube.Use * operator and compound assignment like num *= num;.
+            //Example:
+            //Input → 20, 4
+            //Output → Product = 80, Quotient = 5
+            //Write a C# program to input marks of 5 subjects.Use += operator to calculate total, then find percentage.
+            //Example:
+            //Input → 78, 85, 69, 90, 88
+            //Output → Total = 410, Percentage = 82 %
+            //Write a C# program to swap two numbers using arithmetic operators.
+            //Input → a = 5, b = 10
+            //Output → a = 10, b = 5
+            //Write a C# program to Swap Two Numbers without Third Variable
+
+
+        }
+    }
+}
